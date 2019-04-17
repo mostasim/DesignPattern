2e0f6037aba803dcbcf22d652878b0d5202f4f07
Mostasim Billah <mostasim.billah@mobioapp.com>
2019-04-17 15:48:54 +0600

    builder pattern added

---
+++ src/creational/builder_assembler/Dialog.kt
@@ -0,0 +1,19 @@
+package creational.builder_assembler
+
+class Dialog() {
+    fun showTitle() = println("showing title")
+
+    fun setTitle(text: String) = println("setting title text $text")
+
+    fun setTitleColor(color: String) = println("setting title color $color")
+
+    fun showMessage() = println("showing message")
+
+    fun setMessage(text: String) = println("setting message $text")
+
+    fun setMessageColor(color: String) = println("setting message color $color")
+
+    fun showImage(bitmapBytes: ByteArray) = println("showing image with size ${bitmapBytes.size}")
+
+    fun show() = println("showing dialog $this")
+}
